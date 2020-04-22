b39147249bb52d3f703cf09cd8e88426f7df9a9c
igum-natasha <igum.natasha@gmail.com>
2020-04-22 13:56:08 +0300

    Add graph and histogram to interface.py and create requirements.txt

--- interface.py
+++ interface.py
@@ -1,55 +1,101 @@
 from tkinter import *
 import tkinter.ttk as ttk
-languages = ["Пуш", "До", "Лерм", "Куп", "Бл", "Толс",
-                 "Бу", "Салт", "Пр", "Нек", "Ес", "Кап", "Мо",
-                 "Пре", "Со", "Фе"]
-root = Tk()
-root.title("Анализ литературных произведений")
-root.geometry("780x600+270+20")
+import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
+import pandas as pd
+import matplotlib.transforms as mtransforms
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
 
-nb = ttk.Notebook(width=490,height=430)
-nb.grid(row=2,column=4,columnspan=4, rowspan=4,ipadx=3, ipady=2, sticky=N, padx=2,pady=2)
+languages = ["РџСѓС€", "Р”Рѕ", "Р›РµСЂРј", "РљСѓРї", "Р‘Р»", "РўРѕР»СЃ","Р‘Сѓ", "РЎР°Р»С‚", "РџСЂ", "РќРµРє", "Р•СЃ", "РљР°Рї", "РњРѕ","РџСЂРµ", "РЎРѕ", "Р¤Рµ"]
+datalst = [31, 41, 59, 26, 53, 58, 97, 96, 36]
 
-f1 = ttk.Frame(nb)
-f2 = ttk.Frame(nb)
-f3 = ttk.Frame(nb)
-tab1 = ttk.Frame(nb) 
-nb.add(f1, text='Текст')
-nb.add(f2, text='Гистограмма')
-nb.add(f3, text='График')
+#СЃРѕР·РґР°РЅРёРµ РІРєР»Р°РґРѕРє
+def tabs(name):
+    nb = ttk.Notebook(width=490,height=430)
+    nb.grid(row=2,column=4,columnspan=4, rowspan=4,ipadx=3, ipady=2, sticky=N, padx=2,pady=2)
 
- 
-authors_listbox = Listbox(width=40, height=15)
-vsb = Scrollbar(orient="vertical", command=authors_listbox.yview)
-vsb.grid(row=2, column=2, sticky='ns')
-authors_listbox.configure(yscrollcommand=vsb.set)
-for language in languages:
-    authors_listbox.insert(END, language)
- 
-authors_listbox.grid(row=2,column=0,columnspan=2, ipadx=3, ipady=2, sticky=N, padx=2,pady=2)
- 
-poems_listbox = Listbox(width=40,height=10)
-vsb1 = Scrollbar(orient="vertical", command=poems_listbox.yview)
-vsb1.grid(row=4, column=2, sticky='ns')
-poems_listbox.configure(yscrollcommand=vsb1.set) 
-for language in languages:
-    poems_listbox.insert(END, language)
- 
-poems_listbox.grid(row=4,column=0,columnspan=2, ipadx=3, ipady=2, sticky=N, padx=2,pady=2)
+    f1 = ttk.Frame(nb)
+    name.append(f1)
+    f2 = ttk.Frame(nb)
+    name.append(f2)
+    f3 = ttk.Frame(nb)
+    name.append(f3)
 
-label1 = Label(text="Авторы", fg="#eee", bg="#333")
-label1.grid(row=1,column=0,columnspan=2, ipadx=3, ipady=2, sticky=W, padx=2,pady=2)
-label2 = Label(text="Произведения", fg="#eee", bg="#333")
-label2.grid(row=3,column=0,columnspan=2, ipadx=3, ipady=2, sticky=W, padx=2,pady=2)
-label3 = Label(text="Результат исследования", fg="#eee", bg="#333")
-label3.grid(row=1,column=4,columnspan=4, ipadx=3, ipady=2, sticky=W, padx=2,pady=2)
-but=Button(root,text='Существ')
-but1=Button(root,text='Глагол')
-but2=Button(root,text='Прилагат')
-but3=Button(root,text='Очистить')
-but.grid(row=6,column=4, ipadx=3, ipady=2, padx=1,pady=2)
-but1.grid(row=6,column=5,ipadx=10, ipady=2, padx=1,pady=2)
-but2.grid(row=6,column=6, ipadx=10, ipady=2,  padx=1,pady=2)
-but3.grid(row=6,column=7, ipadx=10, ipady=2, padx=1,pady=2)
+    nb.add(f1, text='РўРµРєСЃС‚')
+    nb.add(f2, text='Р“РёСЃС‚РѕРіСЂР°РјРјР°')
+    nb.add(f3, text='Р“СЂР°С„РёРє')
 
-root.mainloop()
+#РїРѕСЃС‚РѕСЂРµРЅРёРµ РіСЂР°С„РёРєР°
+def graph(f3):
+    f=Figure(figsize=(5,5))
+    a=f.add_subplot(111)
+    a.plot(["РџСѓС€", "Р”Рѕ", "Р›РµСЂРј", "РљСѓРї", "Р‘Р»", "РўРѕР»СЃ","Р‘Сѓ", "РЎР°Р»С‚"],[5,6,4,2,8,6,4,1],color='#228B22')
+    canvas=FigureCanvasTkAgg(f,f3)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+# РїРѕСЃС‚СЂРѕРµРЅРёРµ РіРёСЃС‚РѕРіСЂР°РјРјС‹
+def hist(f2,datalst):
+    ff = Figure(figsize=(5,5), dpi=100)
+    xx = ff.add_subplot(111)
+    ind = np.arange(len(datalst))
+    rects1 = xx.bar(ind, datalst, 0.8,color="#DC143C")
+    canvas = FigureCanvasTkAgg(ff, master=f2)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+
+#listbox СЃ Р°РІС‚РѕСЂР°РјРё
+def listbox_author():
+    authors_listbox = Listbox(width=40, height=15)
+    vsb = Scrollbar(orient="vertical", command=authors_listbox.yview)
+    vsb.grid(row=2, column=2, sticky='ns')
+    authors_listbox.configure(yscrollcommand=vsb.set)
+    for language in languages:
+        authors_listbox.insert(END, language)
+    authors_listbox.grid(row=2,column=0,columnspan=2, ipadx=3, ipady=2, sticky=N, padx=2,pady=2)
+
+#listbox СЃ РїСЂРѕРёР·РІРµРґРµРЅРёСЏРјРё
+def listbox_poems():
+    poems_listbox = Listbox(width=40,height=10)
+    vsb1 = Scrollbar(orient="vertical", command=poems_listbox.yview)
+    vsb1.grid(row=4, column=2, sticky='ns')
+    poems_listbox.configure(yscrollcommand=vsb1.set)
+    for language in languages:
+        poems_listbox.insert(END, language)
+    poems_listbox.grid(row=4,column=0,columnspan=2, ipadx=3, ipady=2, sticky=N, padx=2,pady=2)
+
+#РЅР°Р·РІР°РЅРёСЏ
+def labels():
+    label1 = Label(text="РђРІС‚РѕСЂС‹", fg="#eee", bg="#333")
+    label1.grid(row=1,column=0,columnspan=2, ipadx=3, ipady=2, sticky=W, padx=2,pady=2)
+    label2 = Label(text="РџСЂРѕРёР·РІРµРґРµРЅРёСЏ", fg="#eee", bg="#333")
+    label2.grid(row=3,column=0,columnspan=2, ipadx=3, ipady=2, sticky=W, padx=2,pady=2)
+    label3 = Label(text="Р РµР·СѓР»СЊС‚Р°С‚ РёСЃСЃР»РµРґРѕРІР°РЅРёСЏ", fg="#eee", bg="#333")
+    label3.grid(row=1,column=4,columnspan=4, ipadx=3, ipady=2, sticky=W, padx=2,pady=2)
+
+#РєРЅРѕРїРєРё
+def bottoms(root):
+    but=Button(root,text='РЎСѓС‰РµСЃС‚РІ')
+    but1=Button(root,text='Р“Р»Р°РіРѕР»')
+    but2=Button(root,text='РџСЂРёР»Р°РіР°С‚')
+    but3=Button(root,text='РћС‡РёСЃС‚РёС‚СЊ')
+    but.grid(row=6,column=4, ipadx=3, ipady=2, padx=1,pady=2)
+    but1.grid(row=6,column=5,ipadx=10, ipady=2, padx=1,pady=2)
+    but2.grid(row=6,column=6, ipadx=10, ipady=2,  padx=1,pady=2)
+    but3.grid(row=6,column=7, ipadx=10, ipady=2, padx=1,pady=2)
+
+#РіР»Р°РІРЅР°СЏ С‡Р°СЃС‚СЊ
+if __name__=='__main__':
+    root = Tk()
+    root.title("РђРЅР°Р»РёР· Р»РёС‚РµСЂР°С‚СѓСЂРЅС‹С… РїСЂРѕРёР·РІРµРґРµРЅРёР№")
+    root.geometry("780x600+270+20")
+    tabs_name=[]
+    labels()
+    bottoms(root)
+    listbox_author()
+    listbox_poems()
+    tabs(tabs_name)
+    graph(tabs_name[2])
+    hist(tabs_name[1],datalst)
+    root.mainloop()
