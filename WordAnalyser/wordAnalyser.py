27541624b2e597bf1ef270fa7d4b060eb0c909d2
JGMax <JGMax@users.noreply.github.com>
2020-04-23 22:21:54 +0300

    Improvement of NER module
    Add getting names from polyglot
    Add filter for names from natasha

--- WordAnalyser/wordAnalyser.py
+++ WordAnalyser/wordAnalyser.py
@@ -7,58 +7,73 @@ import codecs
 from natasha import NamesExtractor
 from polyglot.detect import Detector
 from polyglot.text import Text
-
-from WordAnalyser import formateText
+import formatText
 
 with codecs.open('text.txt', encoding='utf-8') as f:
     text = f.read()
     f.close()
 
-
 detector = Detector(text)
 print(detector.language)
 
-text1 = Text(text)
-print(text1.entities)
-
-text = formateText.remove(text, '«»,":;()\r')
+text = formatText.remove(text, '«»,":;()\r')
 
 text = text.replace('\n', ' ')
-text = text.replace('.', ' . ')
-text = text.replace('!', ' ! ')
-text = text.replace('?', ' ? ')
-text = text.replace('ё', 'е')
+text = text.replace('.', ' ')
+text = text.replace('!', ' ')
+text = text.replace('?', ' ')
+# text = text.replace('ё', 'е')
+
+
+polycharacters = []
+polytext = Text(text)
+for entity in polytext.entities:
+    if entity.tag == "I-PER":
+        character = [item for item in list(polytext.words[entity.start: entity.end])
+                     if item.istitle() and len(item) > 1]
+        if character not in polycharacters and character:
+            polycharacters.append(character)
+
+print(polycharacters)
 
 characters = []
 extractor = NamesExtractor()
 matches = extractor(text)
 for match in matches:
-    #character = formateText.strBetween(str(match.fact), "'").title()
-    print(str(match.fact))
-    #if character not in characters:
-        #characters.append(character)
+    character = ''
+    for fact in match.fact:
+        if fact:
+            character += str(fact).title() + ' '
+    character = character.rstrip()
+    if not characters:
+        characters.append(character)
+    else:
+        wasEnter = 0
+        for item in characters:
+            if character in item:
+                wasEnter = 1
+                break
+        if not wasEnter:
+            characters.append(character)
+
+print(characters)
 
 morph = pymorphy2.MorphAnalyzer()
 words = text.split(' ')
 statistic = {}
 posts = []
-index = 0
-for i in words:
-    analyseResult = morph.parse(words[index])[0]
+for word in words:
+    analyseResult = morph.parse(word)[0]
     if analyseResult.tag.POS:
         if analyseResult.tag.POS not in posts:
             posts.append(analyseResult.tag.POS)
             statistic[analyseResult.tag.POS] = 0
 
         statistic[analyseResult.tag.POS] += 1
-        index += 1
     else:
-        words.pop(index)
+        words.remove(word)
 
-index = 0
-for i in posts:
-    print(posts[index] + ' - ' + str(statistic[i]))
-    index += 1
-#print(characters)
-print(words)
+# for post in posts:
+    # print(post + ' - ' + str(statistic[post]))
+# print(words)
 
