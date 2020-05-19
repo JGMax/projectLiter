0409228e929cf0c0675e5155b1eed075e6c7bd42
natasha <igum.natasha@gmail.com>
2020-05-19 14:57:33 +0300

    Bugs: MemoryError with book.txt and KeyError with book1.txt, also index out of range in WordFreq.py in work with any text

--- WordAnalyser/config.py
+++ WordAnalyser/config.py
@@ -1,8 +1,8 @@
 
 import pymorphy2
 
-nerModelPath = './models/slovnet_ner_news_v1.tar'
-nevecModelPath = './models/navec_news_v1_1B_250K_300d_100q.tar'
+nerModelPath = './WordAnalyser/models/slovnet_ner_news_v1.tar'
+nevecModelPath = './WordAnalyser/models/navec_news_v1_1B_250K_300d_100q.tar'
 
 triggerSymbols = ['.', '!', '?', '"', '«', '\n']
 removeSymbols = '»,:;()\r'
