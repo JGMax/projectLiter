4c8e1c1a8117d1f6998a48a5dc9288fd46181db1
natasha <igum.natasha@gmail.com>
2020-05-19 15:03:25 +0300

    Change names of tabs, work with data from WordAnalyser and WordFrequency, add buttom text to print text and buttom Characters to print list of characters, buttom Vocabulary to print vocabulary and draw hist

--- interface.py
+++ interface.py
@@ -6,17 +6,34 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from time import sleep
 import codecs
+from projectLiter.WordAnalyser.results_keys import morph_statistic_key, morph_posts_key, \
+    characters_key, frequency_key, dict_of_words_key
 import threading
 import queue
+from projectLiter.WordAnalyser.word_analyser import text_analysis
 from projectLiter.requests import SearchAboutAuthor, SearchBook
 matplotlib.use("TkAgg")
 author = ['Антон Чехов', 'Лев Толстой', 'Иван Тургенев', 'Николай Гоголь', 'Александр Куприн',
           'Михаил Булгаков', 'Максим Горький', 'Виктор Астафьев', 'Александр Солженицын', 'Федор Достоевский']
 characters = ["Пуш", "До", "Лерм", "Куп", "Бл", "Толс","Бу", "Салт", "Пр", "Нек", "Ес", "Кап", "Мо", "Пре", "Со", "Фе"]
 datalst = [31, 41, 59, 26, 53, 58, 97, 96, 36]
-help_text = 'Выберите автора в первом окне, затем его произведение во втором!!!'
+help_text = "Welcome to the literary analysis application.\n" \
+            "Here you can see the writer's biography, find his poems, and also analyze:\n" \
+            "find characters, top of the most popular words, and also a dictionary of vocabulary.\n" \
+            "First, you must select the author’s name from the top list and select a work \n" \
+            "from the second list."
+help_text1 = 'Here you can find out the main characters of the work, as well as see the frequency\n' \
+             'of their occurrence in each chapter.'
+help_text2 = 'Here you can see the amount of each part of speech in a work, and also see the top of\n' \
+             'the most popular words in a work'
 message = ''
 find = ()
+book=''
+result = {frequency_key: [],
+          characters_key: [],
+          morph_posts_key: [],
+          morph_statistic_key: [],
+          dict_of_words_key: []}
 
 
 def tabs(name):
@@ -24,17 +41,21 @@ def tabs(name):
     nb.grid(row=2, column=4, columnspan=4, rowspan=4, ipadx=3, ipady=2, sticky=N, padx=2,pady=2)
 
     f1 = Canvas(nb)
+    name.append(f1)
+    f2 = Canvas(nb)
+    name.append(f2)
+    #create_combobox(f2)
+    #f3 = ttk.Frame(nb)
+    f3 = Canvas(nb)
+    name.append(f3)
     vsb1 = Scrollbar(orient="vertical", command=f1.yview)
     vsb2 = Scrollbar(orient="horizontal", command=f1.xview)
     vsb1.grid(row=2, column=9, rowspan=6, sticky='ns')
     vsb2.grid(row=6, column=4, columnspan=4, sticky='ew')
-    name.append(f1)
-    f2 = ttk.Frame(nb)
-    name.append(f2)
-    create_combobox(f2)
-    f3 = ttk.Frame(nb)
-    name.append(f3)
-    print_text(f1, help_text)
+
+    print_text(f1, help_text, 10, 'help')
+    print_text(f2, help_text1, 40, 'help1')
+    print_text(f3, help_text2, 40, 'help2')
 
     nb.add(f1, text='Text')
     nb.add(f2, text='Characters')
@@ -52,38 +73,39 @@ def get_active_text(env):
 
 
 def create_combobox(f2):
-    combobox = ttk.Combobox(f2, values=characters,  exportselection=0)
+    combobox = ttk.Combobox(f2, values=result[characters_key],  exportselection=0)
     label1 = Label(f2, text="Выберите персонажа:", fg="#000000")
-    label1.grid(row=2, column=0, columnspan=2, ipadx=3, ipady=2, sticky=W, padx=2, pady=2)
-    combobox.grid(row=2, column=2, columnspan=2, ipadx=3, ipady=2, sticky=N, padx=2, pady=2)
+    label1.grid(row=0, column=2, columnspan=2, ipadx=3, ipady=2, sticky=W, padx=2, pady=2)
+    combobox.grid(row=0, column=4, columnspan=2, ipadx=3, ipady=2, sticky=N, padx=2, pady=2)
     combobox.bind('<<ComboboxSelected>>', get_active_text)
     combobox.current(1)
     return combobox
 
 
-def graph(f3):
-    f = Figure(figsize=(5, 5))
+def graph(f3, array1, array2):
+    f = Figure(figsize=(3, 3))
     a = f.add_subplot(111)
-    a.plot(["Пуш", "До", "Лерм", "Куп", "Бл", "Толс", "Бу", "Салт"], [5, 6, 4, 2, 8, 6, 4, 1], color='#228B22')
+    a.plot(array1, array2, color='#228B22')
     canvas = FigureCanvasTkAgg(f, f3)
     canvas.draw()
-    canvas.get_tk_widget().pack()
+    canvas.get_tk_widget().grid(row=3, column=0, ipadx=3, ipady=2, padx=1, pady=2)
 
 
-def hist(f2, data):
-    ff = Figure(figsize=(5, 5), dpi=100)
+def hist(f2,  array1, array2, x, y):
+    ff = Figure(figsize=(3, 3), dpi=100)
     xx = ff.add_subplot(111)
-    ind = np.arange(len(data))
-    xx.bar(ind, data, 0.8, color="#DC143C")
+    ind = np.arange(len(array2))
+    xx.bar(ind, array2, 0.8, color="#DC143C")
     canvas = FigureCanvasTkAgg(ff, master=f2)
     canvas.draw()
-    canvas.get_tk_widget().pack()
+    canvas.get_tk_widget().grid(row=x+1, column=y, columnspan=7, ipadx=3, ipady=2, padx=1, pady=2)
 
 
-def print_text(f1, text_message):
+def print_text(f, text_message, y, tag):
     global message
-    f1.delete(message)
-    message = f1.create_text(10, 10, anchor=NW, text=text_message, fill="#000000")
+    print(message)
+    f.delete(message)
+    message = f.create_text(10, y, anchor=NW, text=text_message, fill="#000000", tag=tag)
 
 
 def cur_select_authors(evn):
@@ -101,12 +123,15 @@ def cur_select_authors(evn):
         t.join()
         find = que.get()'''
         find = SearchAboutAuthor(value)
-        print_text(tabs_name[0], find[0])
+        label1 = Label(tabs_name[0], text="Biography", fg="#000000")
+        label1.grid(row=0, column=0, columnspan=5, ipadx=3, ipady=2, sticky=W, padx=2, pady=2)
+        tabs_name[0].delete('help')
+        print_text(tabs_name[0], find[0], 30, 'biogr')
         print_poems(listbox_poems(), find[1])
 
 
 def cur_select_poems(evn):
-    global tabs_name
+    global tabs_name, book
     wid = evn.widget
     i, value = 0, ''
     if wid.curselection() != ():
@@ -120,9 +145,18 @@ def cur_select_poems(evn):
         t.join()
         book = que1.get()'''
         book = SearchBook(value, find[1], find[2])
-        with codecs.open(book, encoding='utf-8') as file:
-            text = file.read()
-        print_text(tabs_name[0], text)
+        analyser(book, 'ru')
+        but = Button(tabs_name[0], text='Book', command=print_book)
+        but.grid(row=0, column=6, ipadx=3, ipady=2, padx=1, pady=2)
+
+
+def print_book():
+    label1 = Label(tabs_name[0], text="   Text    ", fg="#000000")
+    label1.grid(row=0, column=0, columnspan=5, ipadx=3, ipady=2, sticky=S, padx=2, pady=2)
+    with codecs.open(book, encoding='utf-8') as file:
+        text = file.read()
+    tabs_name[0].delete('biogr')
+    print_text(tabs_name[0], text, 50, 'text')
 
 
 def listbox_author():
@@ -134,6 +168,11 @@ def listbox_author():
     for writer in author:
         authors_listbox.insert(END, writer)
     authors_listbox.grid(row=2, column=0, columnspan=2, ipadx=3, ipady=2, sticky=N, padx=2, pady=2)
+
+
+def analyser(book, language):
+    global result
+    result = text_analysis(book, language)
 
 
 def listbox_poems():
@@ -154,6 +193,39 @@ def print_poems(poems_listbox, books):
         poems_listbox.insert(END, row)
 
 
+def print_characters():
+    answer = ''
+    label1 = Label(tabs_name[1], text="Найденные персонажи:", fg="#000000")
+    label1.grid(row=1, column=0, columnspan=5, ipadx=3, ipady=2, sticky=W, padx=2, pady=2)
+    tabs_name[1].delete('help1')
+    if result[characters_key]:
+        for character in result[characters_key]:
+            answer += str(character)
+            answer += '\n'
+        print_text(tabs_name[1], answer, 70, 'charact')
+        create_combobox(tabs_name[1])
+    else:
+        print_text(tabs_name[1], 'Not found!', 70, 'not1')
+
+
+def print_vocab():
+    array1, array2 = [], []
+    answer = ''
+    label1 = Label(tabs_name[2], text="Найденные части речи:", fg="#000000")
+    label1.grid(row=1, column=0, ipadx=3, ipady=2, sticky=W, padx=2, pady=2)
+    tabs_name[2].delete('help2')
+    if result[morph_posts_key]:
+        for post in result[morph_posts_key]:
+            array1.append(post)
+            array2.append(int(result[morph_statistic_key][post]))
+            answer += str(post + ' - ' + str(result[morph_statistic_key][post]))
+            answer += '\n'
+        print_text(tabs_name[2], answer, 70, 'vocab')
+        hist(tabs_name[2], array1, array2, 1, 1)
+    else:
+        print_text(tabs_name[2], 'Not found!', 70, 'not2')
+
+
 def labels():
     label1 = Label(text="Авторы", fg="#eee", bg="#333")
     label1.grid(row=1, column=0, columnspan=2, ipadx=3, ipady=2, sticky=W, padx=2, pady=2)
@@ -164,14 +236,14 @@ def labels():
 
 
 def bottoms(r):
-    but = Button(r, text='Существ')
-    but1 = Button(r, text='Глагол')
-    but2 = Button(r, text='Прилагат')
-    but3 = Button(r, text='Очистить')
-    but.grid(row=7, column=4, ipadx=3, ipady=2, padx=1, pady=2)
-    but1.grid(row=7, column=5, ipadx=10, ipady=2, padx=1, pady=2)
-    but2.grid(row=7, column=6, ipadx=10, ipady=2,  padx=1, pady=2)
-    but3.grid(row=7, column=7, ipadx=10, ipady=2, padx=1, pady=2)
+    but = Button(r[1], text='Characters', command=print_characters)
+    but1 = Button(r[2], text='Vocabulary', command=print_vocab)
+    but2 = Button(r[2], text='Прилагат')
+    but3 = Button(r[2], text='Очистить')
+    but.grid(row=0, column=0, ipadx=3, ipady=2, padx=1, pady=2)
+    but1.grid(row=0, column=0, ipadx=10, ipady=2, padx=1, pady=2)
+    but2.grid(row=0, column=1, ipadx=10, ipady=2,  padx=1, pady=2)
+    but3.grid(row=0, column=2, ipadx=10, ipady=2, padx=1, pady=2)
 
 
 def loading(r):
@@ -197,7 +269,7 @@ if __name__ == '__main__':
     listbox_author()
     listbox_poems()
     tabs(tabs_name)
-    bottoms(root)
-    create_combobox(tabs_name[1])
+    bottoms(tabs_name)
+    #create_combobox(tabs_name[1])
     #main()
     root.mainloop()
