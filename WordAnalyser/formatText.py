380e892519fc2aa8f255e00afc6b97ddea153d79
JGMax <JGMax@users.noreply.github.com>
2020-06-06 21:41:16 +0300

    Fix mistake

--- WordAnalyser/formatText.py
+++ WordAnalyser/formatText.py
@@ -6,7 +6,6 @@ def remove(value, deleteChars):
 
 def formatText(editText, stringToRemove, listOfTriggers):
     editText = remove(editText, stringToRemove)
-    editText = editText.replace('\n', ' ')
     for item in listOfTriggers:
         editText = editText.replace(item, f' {item} ')
     return editText
